ec6610088845e0562c2d4a9c77f966cdea2129d1
kepler <email@sample.com>
2023-11-23 11:29:46 +0800

    初始化

--- utils/request.js
+++ utils/request.js
@@ -4,9 +4,9 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 
 // create an axios instance
 const service = axios.create({
-	 baseURL: "https://t44o902052.zicp.fun", //  线上接口地址(微信、HBX内置浏览器)
+	 // baseURL: "https://t44o902052.zicp.fun", //  线上接口地址(微信、HBX内置浏览器)
 	// baseURL: "http://app.youlai.tech/prod-api", // 线上接口地址(谷歌等有跨域限制浏览器)
-	// baseURL: "http://localhost:9999", // 本地开发环境地址
+	baseURL: "http://192.168.1.112:9999", // 本地开发环境地址
 	withCredentials: true, // send cookies when cross-domain requests
 	timeout: 10000 // request timeout
 })
